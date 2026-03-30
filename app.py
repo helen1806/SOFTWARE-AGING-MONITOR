af291dd6b85ce85fcc4ce3a603d8c5ab222a7c4c
helen <helenmarys1023@gmail.com>
2026-03-30 11:48:29 +0530

    database url updated

--- app.py
+++ app.py
@@ -5,10 +5,12 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from datetime import datetime, timedelta
 from models import db, Website, MonitoringCheck, Incident, SubPage
 from monitor import monitor_website, get_website_stats
+from dotenv import load_dotenv
 
+load_dotenv()
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-production') 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Root@localhost:5433/Software aging'
+app.config['SQLALCHEMY_DATABASE_URI'] =os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 
@@ -295,7 +297,7 @@ def chart_data(website_id):
 with app.app_context():
     db.create_all()
 
-if __name__ == '__main__': #required block to prevent from being used when imported
+if __name__ == '__main__': 
     start_scheduler()
     app.run(host='0.0.0.0', port=5000, debug=True)
 
